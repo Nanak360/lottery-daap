5debd9b5f6a4bcb709f8c0f6029b423a308744f7
Nanak <nanakbandyopadhyay@hotmail.com>
2021-12-02 10:12:19 +0530

    add material ui to client side

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
 
 function Nav() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (<div>
+      
+  </div>);
 }
 
-export default Nav
+export default Nav;
